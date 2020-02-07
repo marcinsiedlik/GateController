fe146db0a8bedd58706278f82e01cbc0101f8651
Marcin <marcin.siedlik97@gmail.com>
2020-02-07 11:49:04 +0100

    Handle gate action requestes

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,10 @@
 FirebaseConnection connection;
 GateRemote remote(D5, D6);
 
+void handleOpenGateRequests();
+void handleCloseGateRequests();
+void delayedGateStateReset();
+
 void setup()
 {
   Serial.begin(9600);
@@ -13,4 +17,37 @@ void setup()
 
 void loop()
 {
+  handleOpenGateRequests();
+  handleCloseGateRequests();
+
+  //Added to save some mobile data
+  delay(2000);
+}
+
+void handleOpenGateRequests()
+{
+  if (connection.getGateOpenRequest())
+  {
+    connection.setGateOpenValue(false);
+    remote.pressOpenButton();
+    connection.setGateState(GateState::OPENING);
+    delayedGateStateReset();
+  }
+}
+
+void handleCloseGateRequests()
+{
+  if (connection.getGateCloseRequest())
+  {
+    connection.setGateCloseValue(false);
+    remote.pressCloseButton();
+    connection.setGateState(GateState::CLOSING);
+    delayedGateStateReset();
+  }
+}
+
+void delayedGateStateReset()
+{
+  delay(16000);
+  connection.setGateState(GateState::NOT_MOVING);
 }
