66452701183a71baa6a29e8348b861815b040479
Marcin Siedlik <marcin.siedlik97@gmail.com>
2023-09-25 17:43:58 +0200

    Setup settings with version catalog and buildSrc

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,9 @@
+@file:Suppress("UnstableApiUsage")
+
+import com.android.build.api.dsl.SettingsExtension
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
 pluginManagement {
   repositories {
     google()
@@ -5,13 +11,30 @@ pluginManagement {
     gradlePluginPortal()
   }
 }
+
+plugins {
+  id("com.android.settings") version "8.1.1"
+}
+
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
     google()
     mavenCentral()
   }
+
+  versionCatalogs {
+    create("libs") {
+      from(files("gradle/dependencies.toml"))
+    }
+  }
+}
+
+configure<SettingsExtension> {
+  minSdk = 29
+  compileSdk = 34
 }
 
 rootProject.name = "GateController"
+
 include(":app")
