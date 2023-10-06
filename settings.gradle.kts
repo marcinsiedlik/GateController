869ce140a98d6352402a166c5a459ced5dd6ba8d
Marcin Siedlik <marcin.siedlik97@gmail.com>
2023-10-06 22:02:46 +0200

    Include build-logic build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,19 +1,14 @@
 @file:Suppress("UnstableApiUsage")
 
-import com.android.build.api.dsl.SettingsExtension
-
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
+  includeBuild("build-logic")
   repositories {
     google()
     mavenCentral()
     gradlePluginPortal()
   }
-}
-
-plugins {
-  id("com.android.settings") version "8.1.1"
 }
 
 dependencyResolutionManagement {
@@ -22,17 +17,6 @@ dependencyResolutionManagement {
     google()
     mavenCentral()
   }
-
-  versionCatalogs {
-    create("libs") {
-      from(files("gradle/dependencies.toml"))
-    }
-  }
-}
-
-configure<SettingsExtension> {
-  minSdk = 29
-  compileSdk = 34
 }
 
 rootProject.name = "GateController"
