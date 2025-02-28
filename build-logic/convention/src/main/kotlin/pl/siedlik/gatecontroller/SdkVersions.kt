acdca6376eb7704ff67ac4fdeb55bb5a0a85cc49
Marcin Siedlik <marcin.siedlik97@gmail.com>
2025-02-28 17:07:15 +0100

    Update all libs

--- build-logic/convention/src/main/kotlin/pl/siedlik/gatecontroller/SdkVersions.kt
+++ build-logic/convention/src/main/kotlin/pl/siedlik/gatecontroller/SdkVersions.kt
@@ -3,6 +3,6 @@ package pl.siedlik.gatecontroller
 import com.android.build.api.dsl.CommonExtension
 
 fun CommonExtension<*, *, *, *, *, *>.configureSdkVersions() {
-  compileSdk = 34
+  compileSdk = 35
   defaultConfig.minSdk = 29
 }
