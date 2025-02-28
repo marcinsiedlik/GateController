175104b4a0a5cdf48384575e1a56eae999a290f4
Marcin Siedlik <marcin.siedlik97@gmail.com>
2025-02-28 17:33:27 +0100

    Update app version [2.6]

--- build-logic/convention/src/main/kotlin/pl/siedlik/gatecontroller/AppVersions.kt
+++ build-logic/convention/src/main/kotlin/pl/siedlik/gatecontroller/AppVersions.kt
@@ -10,7 +10,7 @@ enum class AppPlatform(val versionSuffix: Int) {
 object AppVersion {
 
   private const val versionMajor = 2
-  private const val versionMinor = 5
+  private const val versionMinor = 6
 
   const val applicationId = "pl.siedlik.gatecontroller"
   const val versionName = "$versionMajor.$versionMinor"
